186741db76e84613aecd66c149c5c1db2ba3b1ba
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-04 20:00:52 -0500

    feat: initial attempt at animation event listening

---
+++ src/animeventhandler.cpp
@@ -0,0 +1,40 @@
+#include "PCH.h"
+
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace std::literals;
+
+RE::BSEventNotifyControl MSCO::AnimationEventSink::ProcessEvent(
+    const RE::BSAnimationGraphEvent* a_event,
+    RE::BSTEventSource<RE::BSAnimationGraphEvent>*)
+{   
+    //abort if no event or no event holder
+    if (!a_event || !a_event->holder) {
+        return RE::BSEventNotifyControl::kContinue;
+    }
+    //abort if no actor
+    auto* actor = a_event->holder->As<RE::Actor>();
+    if (!actor) {
+        return RE::BSEventNotifyControl::kContinue;
+    }
+    const auto& tag = a_event->tag;  // BSFixedString
+    constexpr auto kLeftTag = "lh_msco_cast"sv;
+    constexpr auto kRightTag = "rh_msco_cast"sv;
+    constexpr auto kDualTag = "dc_msco_cast"sv;
+
+    if (tag == kLeftTag) {
+        log::info("{} has Left Casted.", actor->GetName());
+        //logger::info("LH event for {}", actor->GetName());
+        //CastEquippedHand(actor, /*left*/ true, /*dual*/ false);
+    } else if (tag == kRightTag) {
+        log::info("{} has Right Casted.", actor->GetName());
+        //logger::info("RH event for {}", actor->GetName());
+        //CastEquippedHand(actor, /*left*/ false, /*dual*/ false);
+    } else if (tag == kDualTag) {
+        log::info("{} has Dual Casted.", actor->GetName());
+        //logger::info("DC event for {}", actor->GetName());
+        //CastEquippedHand(actor, /*left*/ true, /*dual*/ true);
+    }
+
+    return RE::BSEventNotifyControl::kContinue;
+}
