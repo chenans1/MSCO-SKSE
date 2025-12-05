f31474b967ee8de1e84940601f97e81476eda42e
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-05 13:26:57 -0500

    chore: clean up magichandler.h

--- src/magichandler.h
+++ src/magichandler.h
@@ -3,15 +3,9 @@
 #include "PCH.h"
 
 namespace MSCO::Magic {
-
-	/*enum class Hand {
-		Left,
-		Right
-	};*/
     //enum class Hand : std::uint8_t { Right = 0, Left = 1 };
 
     enum class Hand : std::uint8_t { Right = 1, Left = 0 };
 	RE::MagicItem* GetEquippedSpellHand(RE::Actor* actor, Hand hand);
-
 	bool CastEquippedHand(RE::Actor* actor, Hand hand, bool dualCast);
 }
