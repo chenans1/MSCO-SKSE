8bb91e08231ef6a2723d319835d2e92efdc7aba1
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-07 00:14:17 -0500

    feat: headers for inputhandler

--- src/inputhandler.h
+++ src/inputhandler.h
@@ -3,8 +3,32 @@
 #include "PCH.h"
 #include "magichandler.h"
 
-namespace MSCO::InputHandler {
+namespace MSCO {
     class InputHandler : public RE::BSTEventSink<RE::InputEvent*> {
+        public:
+            using EventResult = RE::BSEventNotifyControl;
 
+            static InputHandler& GetSingleton() { 
+                static InputHandler singleton;
+                return singleton;
+            }
+
+            EventResult ProcessEvent(
+                RE::InputEvent* const* a_events, 
+                RE::BSTEventSource<RE::InputEvent*>* a_eventSource) 
+                override;
+
+        private:
+            InputHandler() = default;
+            ~InputHandler() = default;
+            InputHandler(const InputHandler&) = delete;
+            InputHandler(InputHandler&&) = delete;
+            InputHandler& operator=(const InputHandler&) = delete;
+            InputHandler& operator=(InputHandler&&) = delete;
+
+            //helpers
+            bool IsValidPlayerState(RE::PlayerCharacter* player) const;
+            //bool HasEnoughMagicka(RE::PlayerCharacter* player, MSCO::Magic::Hand hand, bool dualCast) const;
+            void SendAnimEvent(RE::PlayerCharacter* player, std::string_view eventName) const;
     };
 }
