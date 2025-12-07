1860d15fb9dd32d4a0b2c920d72a23ff5438212a
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2025-12-07 00:05:20 -0500

    feat: initial attempts at handling player input

---
+++ src/inputhandler.cpp
@@ -0,0 +1,7 @@
+#include "PCH.h"
+#include "inputhandler.h"
+#include "magichandler.h"
+
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace std::literals;
---
+++ src/inputhandler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "PCH.h"
+#include "magichandler.h"
+
+namespace MSCO::InputHandler {
+    class InputHandler : public RE::BSTEventSink<RE::InputEvent*> {
+
+    };
+}
